eac40eceaf73d041387dc007c8a1a70aad9149d6
ELIJAH K NSUBUGA <elijahknsubuga@gmail.com>
2020-03-20 12:15:59 +0300

    working on the data pane

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.post('*', (req, res) => {
     2. Western
     3. Eastern
     4. Northern
-    5. Southern
     0. Cancel`
 
     res.send(response)
@@ -53,7 +52,6 @@ app.post('*', (req, res) => {
     2. Western
     3. Eastern
     4. Northern
-    5. Southern
     0. Cancel`
 
     res.send(response)
@@ -68,12 +66,93 @@ app.post('*', (req, res) => {
 
   }
 
-  else if (text == '1*1') {
-    // Business logic for public central region response
-    let response = `CON Choose a hospital you want to view
+   else if (text == '1*1*2') {
+    // Business logic for public Mengo hospital response
+    let response = `CON Mengo hospital services
 
-    1. Mulago hospital
-    2. Mengo hospital`
+    1. Location
+    2. services
+    0. Cancel`
+
+    res.send(response)
+
+  } 
+
+  else if (text == '1*1*1*1') {
+    // Business logic for Mengo hospital location response
+    let response = `END Mulago hospital Location
+
+    Kampala Uganda`
+
+    res.send(response)
+
+  } 
+
+  else if (text == '1*1*1*2') {
+    // Business logic for public Mengo hospitalservices response
+    let response = `END Mulago hospital services
+
+    1. sychiatry
+    2. Ear
+    3. Nose
+    4. Throat (ENT)
+    5. Radiology
+    6. Pathology
+    7. Ophthalmology`
+
+    res.send(response)
+
+  } 
+
+   else if (text == '1*1*1*0') {
+    // Business logic for Mengo mulago hospital cancel response
+    let response = `END Exiting`
+
+    res.send(response)
+
+  } 
+
+  else if (text == '1*1*1') {
+    // Business logic for public mulago hospital response
+    let response = `CON Mulago hospital services
+
+    1. Location
+    2. services
+    0. Cancel`
+
+    res.send(response)
+
+  } 
+
+  else if (text == '1*1*1*1') {
+    // Business logic for mulago hospital location response
+    let response = `END Mulago hospital Location
+
+    Kampala Uganda`
+
+    res.send(response)
+
+  } 
+
+  else if (text == '1*1*1*2') {
+    // Business logic for public mulago hospitalservices response
+    let response = `END Mulago hospital services
+
+    1. sychiatry
+    2. Ear
+    3. Nose
+    4. Throat (ENT)
+    5. Radiology
+    6. Pathology
+    7. Ophthalmology`
+
+    res.send(response)
+
+  } 
+
+   else if (text == '1*1*1*0') {
+    // Business logic for public mulago hospital cancel response
+    let response = `END Exiting`
 
     res.send(response)
 
@@ -105,16 +184,6 @@ app.post('*', (req, res) => {
     let response = `CON Choose a hospital you want to view
 
     1. Apac hospital`
-
-    res.send(response)
-
-  } 
-
-  else if (text == '1*5') {
-    // Business logic for public southern region response
-    let response = `CON Choose a hospital you want to view
-
-    1. Masaka hospital`
 
     res.send(response)
 
@@ -163,17 +232,6 @@ app.post('*', (req, res) => {
 
   else if (text == '2*4') {
     // Business logic for private northern region response
-    let response = `CON Choose a hospital you want to view
-
-    1. CoRSU hospital
-    2. Mildmay health center`
-
-    res.send(response)
-
-  } 
-
-  else if (text == '2*5') {
-    // Business logic for private southern region response
     let response = `CON Choose a hospital you want to view
 
     1. CoRSU hospital
