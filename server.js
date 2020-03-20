d2c61254b844c49f187dd5b159d2e6a12fd0ae60
ELIJAH K NSUBUGA <elijahknsubuga@gmail.com>
2020-03-20 17:30:10 +0300

    working on the data pane

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.post('*', (req, res) => {
 
   let {sessionId, serviceCode, phoneNumber, text} = req.body
 
+
+
+//this is the begining
+
+
+
   if (text == '') {
     // This is the first request. letting the user pick a region where the hospital they want is located
     let response = `CON Please select the type of hospital
@@ -29,6 +35,11 @@ app.post('*', (req, res) => {
     res.send(response)
 
   } 
+
+
+  //public section
+  //1
+
 
   else if (text == '1') {
     // Business logic for public response
@@ -66,6 +77,11 @@ app.post('*', (req, res) => {
 
   }
 
+
+  //Public
+  //central
+  //1/1
+
   else if (text == '1*1') {
     // Business logic for public western region response
     let response = `CON Choose a hospital you want to view
@@ -76,6 +92,12 @@ app.post('*', (req, res) => {
     res.send(response)
 
   } 
+
+
+  //public
+  //central
+  //1/1/2
+  //mengo hospital
 
    else if (text == '1*1*2') {
     // Business logic for public Mengo hospital response
@@ -91,7 +113,7 @@ app.post('*', (req, res) => {
 
   else if (text == '1*1*2*1') {
     // Business logic for Mengo hospital location response
-    let response = `END Mulago hospital Location
+    let response = `END Mengo hospital Location
 
     Kampala Uganda`
 
@@ -101,27 +123,62 @@ app.post('*', (req, res) => {
 
   else if (text == '1*1*2*2') {
     // Business logic for public Mengo hospitalservices response
-    let response = `END Mulago hospital services
+    let response = `CON Mengo hospital services
 
-    1. sychiatry
-    2. Ear
-    3. Nose
-    4. Throat (ENT)
-    5. Radiology
-    6. Pathology
-    7. Ophthalmology`
+    1. Specialised
+    2. Medicine
+    3. Surgery
+    4. Pharmacy
+    5. Laboratory
+    6. Private out patient
+    7. Generaloutpatient
+    8. Nursing department
+    9. Corporate Social responsibility`
 
     res.send(response)
 
   } 
 
-   else if (text == '1*1*2*0') {
+  else if (text == '1*1*2*1'){
+
+    let response = `END 
+
+    1. Eye clinic
+    2. Dental clinic
+    3. Counseling and homecare
+    4. Orthopedics
+    5. Obstetrics  and gynaecology
+    6. Physiotherapy
+    7. Mental health`
+
+    res.send(response)
+
+  }
+
+  else if (text == '1*1*2*2'){
+
+    let response = `END 
+
+    1. Wards`
+
+    res.send(response)
+
+  }
+
+   else if (text == '1*1*2*0'|| '1*1*2*3' || '1*1*2*4' || '1*1*2*5' || '1*1*2*6' || '1*1*2*8' || '1*1*2*9' ) {
     // Business logic for Mengo mulago hospital cancel response
     let response = `END Exiting`
 
     res.send(response)
 
   } 
+
+
+  //public
+  //central
+  //1/1/2
+  //mulago hospital
+
 
   else if (text == '1*1*1') {
     // Business logic for public mulago hospital response
@@ -139,7 +196,7 @@ app.post('*', (req, res) => {
     // Business logic for mulago hospital location response
     let response = `END Mulago hospital Location
 
-    Kampala Uganda`
+    Mulago, Kampala, Uganda`
 
     res.send(response)
 
